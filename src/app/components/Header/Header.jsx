58140d95ce640c75442df784dc1276e17b6689eb
AlenaBis <9642444600a@gmail.com>
2021-11-16 13:08:54 +0300

    header modile

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { ReactComponent as Cola } from '../../assets/icons/Logo_cola.svg';
 import { ReactComponent as Delivery } from '../../assets/icons/Logo_delivery.svg';
+import { ReactComponent as Feat } from '../../assets/icons/Logo_ft.svg';
+import Button from '../Button/Button';
 
 import styles from './Header.module.scss';
 
@@ -11,15 +13,16 @@ const MainPage = () => {
 			<div className={styles.header__logo}>
 				<img className={styles.header__band} src={require('../../assets/images/Header_band.png').default} alt="band" />
 				<div className={styles.header__logoWrap}>
-					<Cola />
-					<span className={styles.header__feat} />
-					<Delivery />
+					<Cola className={styles.header__logoCola} />
+					<Feat className={styles.header__Logofeat} />
+					<Delivery className={styles.header__logoDelivery} />
 				</div>
 			</div>
 			<h1 className={styles.header__title}>Погрузитесь в волшебную атмосферу </h1>
 			<p className={styles.header__subTitle}>C advent-календарем, мини-играми и 
                 приятными подарками
 			</p>
+			<Button content="Победители" className={styles.header__button_winners} />
 			<img className={styles.header__bandBottom} src={require('../../assets/images/Header_band_bottom.png').default} alt="band" />
 		</header>
 	);
