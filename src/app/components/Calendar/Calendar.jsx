44b3d8b3a0035506354aa65326c6c2dfc4c25b13
Roman Kashigin <r.kashigin@yandex.ru>
2021-11-30 19:33:36 +0500

    Modal condition fixed

--- src/app/components/Calendar/Calendar.jsx
+++ src/app/components/Calendar/Calendar.jsx
@@ -31,7 +31,7 @@ const Calendar = observer(() => {
                         date={el.day}
                         img={el.img}
                         className={classNames(styles[`calendarDay_${el.day}`], {
-                            [styles.calendarDay_current]: RootStore.myGamesCompleted === idx,
+                            [styles.calendarDay_current]: RootStore.myGamesCompleted - 1 === idx,
                             [styles.calendarDay_pastDay]: false /* isPast(new Date(2021, 11, idx + 1)) */,
                             [styles.calendarDay_futureDay]: isFuture(new Date(2021, 11, idx + 1))
                         })}
