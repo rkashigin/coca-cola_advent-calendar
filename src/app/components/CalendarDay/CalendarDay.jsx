9e333eb92d646b601d3feab938b048e2ebf49860
Roman Kashigin <r.kashigin@yandex.ru>
2021-12-01 13:10:02 +0500

    Modal closing fixed

--- src/app/components/CalendarDay/CalendarDay.jsx
+++ src/app/components/CalendarDay/CalendarDay.jsx
@@ -46,15 +46,14 @@ const CalendarDay = observer(
         const [loadedPromocode, setLoadedPromocode] = React.useState({ 1: '', 12: '' });
 
         const handleClickOpen = () => {
-            if (RootStore.user.id) {
-                // if (isDayActive(date)) {
-                setOpen(true);
-                // }
-            } else {
-                RootStore.setOauthOpen(true);
-            }
+            // if (RootStore.user.id) {
+            // if (isDayActive(date)) {
+            setOpen(true);
+            // }
+            // } else {
+            //     RootStore.setOauthOpen(true);
+            // }
         };
-
         const handleClose = () => {
             setOpen(false);
         };
