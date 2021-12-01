929ae091d3980cbbe541352c457423ba9951ed23
Roman Kashigin <r.kashigin@yandex.ru>
2021-12-01 14:33:03 +0500

    Day condition fixed and added

--- src/app/helpers/isDayActive.js
+++ src/app/helpers/isDayActive.js
@@ -1,8 +1,5 @@
-import { isFuture, isPast } from 'date-fns';
 import { RootStore } from '../stores/RootStore';
 
 export default function isDayActive(date) {
-    if (isPast(new Date(2021, 11, date))) return true;
-
-    return !(date > RootStore.myGamesCompleted || isFuture(new Date(2021, 11, date)));
+    return date <= RootStore.myGamesCompleted;
 }
