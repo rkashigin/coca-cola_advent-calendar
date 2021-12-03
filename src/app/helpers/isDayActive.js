5229de8d46b943f4e6244ce318c055a04bbcf531
Roman Kashigin <r.kashigin@yandex.ru>
2021-12-03 15:12:21 +0500

    IsDayActive fix

--- src/app/helpers/isDayActive.js
+++ src/app/helpers/isDayActive.js
@@ -2,7 +2,9 @@ import { isFuture } from 'date-fns';
 import { RootStore } from '../stores/RootStore';
 
 export default function isDayActive(date) {
-    if (isFuture(new Date(2021, 11, date))) return false;
+    // if (isFuture(new Date(2021, 11, date))) return false;
 
-    return date <= RootStore.myGamesCompleted;
+    // return date <= RootStore.myGamesCompleted;
+
+    return date === 1;
 }
